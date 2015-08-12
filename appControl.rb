7d9fe133ac3a3217e0c88319b2c0d9414d7b0ec4
Whasame <coolmaxalt@gmail.com>
2015-08-12 02:30:05 +0000

    added text me form and finished all recent porjects

--- appControl.rb
+++ appControl.rb
@@ -1,4 +1,6 @@
+require 'mailgun'
 require 'bundler'
+require 'twilio-ruby' 
 Bundler.require
 
 require './models/model.rb'
@@ -7,6 +9,31 @@ class MyApp < Sinatra::Base
 	
 	get '/' do
 		erb :index
+	end
+	
+	post '/text' do
+		@body = "Sent From: #{params[:returnName]} (#{params[:returnNum]})
+
+
+#{params[:body]}"
+
+		account_sid = "ACe30ea6a63d05386cb6cac9a48615cd8e" 
+		auth_token = "7ae7a566fd115e1e450986cb36ce315b" 
+
+		client = Twilio::REST::Client.new account_sid, auth_token 
+ 
+		from = "+13477457924"
+		
+		friends = { "+15417052920" => "User" } 
+		friends.each do |key, value|
+		client.account.messages.create( 
+			:from => from, 
+			:to => key, 
+			:body => "#{@body}" 
+		) 
+			puts "Sent message from #{params[:returnName]}"
+			redirect '/'
+		end 
 	end
 	
 end
